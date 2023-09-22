e8e30028dc7acf25932fd03f02dc3cde3f5ddd66
tomiMayorga <tomasmayorga18@gmail.com>
2023-09-22 15:04:46 -0300

    Incorpore la configuracion y si quiere dar de alta o baja

--- controlDeAcceso/controlDeAcceso.ino
+++ controlDeAcceso/controlDeAcceso.ino
@@ -1,17 +1,11 @@
 /*
  * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read new NUID from a PICC to serial.
+ * LIBRERIA ARDUINO MFRC522 by GithubCommunity.
  * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+ * Libreria: https://github.com/miguelbalboa/rfid
  * 
  * Example sketch/program showing how to the read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
  * Reader on the Arduino SPI interface.
- * 
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the type, and the NUID if a new card has been detected. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
  * 
  * @license Released into the public domain.
  * 
@@ -35,6 +29,7 @@
 #define RST_PIN 9
 #define CANTIDAD_LLAVES 20
 
+
 MFRC522 rfid(SS_PIN, RST_PIN);  // Instance of the class
 
 MFRC522::MIFARE_Key key;
@@ -43,6 +38,7 @@ MFRC522::MIFARE_Key key;
 byte tarjetaLeida[4];
 byte tarjetasHabilitadas[CANTIDAD_LLAVES][4] = { { 154, 175, 242, 129 } };
 bool banderaTarjetaHabilitada;
+//sirve para dar de alta y dar de baja
 int cantLlavesHabilitadas;
 
 void setup() {
@@ -57,81 +53,105 @@ void setup() {
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
   Serial.print(F("Using the following key:"));
   printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+
+  Serial.println(tarjetasHabilitadas[0][0]);
+  Serial.println(tarjetasHabilitadas[0][1]);
+  Serial.println(tarjetasHabilitadas[0][2]);
+  Serial.println(tarjetasHabilitadas[0][3]);
 }
 
 void loop() {
 
   // Look for new cards
-  if (!rfid.PICC_IsNewCardPresent())
-    return;
+  if (rfid.PICC_IsNewCardPresent()) {
+    
+    // Verify if the NUID has been readed
+    if (!rfid.PICC_ReadCardSerial()) {
+      Serial.print("hola2");
+      return;
+    }
+    Serial.print(F("PICC type: "));
+    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+    Serial.println(rfid.PICC_GetTypeName(piccType));
 
-  // Verify if the NUID has been readed
-  if (!rfid.PICC_ReadCardSerial())
-    return;
-
-  Serial.print(F("PICC type: "));
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-  Serial.println(rfid.PICC_GetTypeName(piccType));
-
-  // Check is the PICC of Classic MIFARE type
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-    Serial.println(F("Your tag is not of type MIFARE Classic."));
-    return;
-  }
-
-  if (rfid.uid.uidByte[0] != tarjetaLeida[0] || rfid.uid.uidByte[1] != tarjetaLeida[1] || rfid.uid.uidByte[2] != tarjetaLeida[2] || rfid.uid.uidByte[3] != tarjetaLeida[3]) {
-    Serial.println(F("A new card has been detected."));
-
-    // Store NUID into nuidPICC array
-    for (byte i = 0; i < 4; i++) {
-      tarjetaLeida[i] = rfid.uid.uidByte[i];
+    // Check is the PICC of Classic MIFARE type
+    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+      Serial.println(F("Your tag is not of type MIFARE Classic."));
+      return;
     }
 
-    Serial.println(F("The NUID tag is:"));
-    Serial.print(F("In hex: "));
-    printHex(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-    Serial.print(F("In dec: "));
-    printDec(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-  } else Serial.println(F("Card read previously."));
+    if (rfid.uid.uidByte[0] != tarjetaLeida[0] || rfid.uid.uidByte[1] != tarjetaLeida[1] || rfid.uid.uidByte[2] != tarjetaLeida[2] || rfid.uid.uidByte[3] != tarjetaLeida[3]) {
+      Serial.println(F("Se detecto una nueva tarjeta/tag."));
+
+      // Store NUID into nuidPICC array //Preguntarle a Lean si se guarda aca la tarjeta.
+      for (byte i = 0; i < 4; i++) {
+        tarjetaLeida[i] = rfid.uid.uidByte[i];
+      }
+
+      Serial.println(F("El NUID tag es:"));
+      Serial.print(F("En hex: "));
+      printHex(rfid.uid.uidByte, rfid.uid.size);
+      Serial.println();
+      Serial.print(F("En dec: "));
+      printDec(rfid.uid.uidByte, rfid.uid.size);
+      Serial.println();
+    } else Serial.println(F("Tarjeta/Tag leida previamente."));
 
 
-  //Guardar llave nueva
-  /*
+    //Guardar llave nueva
+    /*
   for(int fila = 0; fila < 2 ; fila++){
     for(int columna = 0; columna < 2; columna++){
       matriz[fila][columna] = tarjetaHabilitada[];
     }
   }
 
-*/
+  */
 
-  //Pregunto si la llave leida esta habilitada
-  for (int fila = 0; fila < 20; fila++) {
-    banderaTarjetaHabilitada = true;
-    for (byte i = 0; i < 4; i++) {
+    //Pregunto si la llave leida esta habilitada
+    for (int fila = 0; fila < CANTIDAD_LLAVES; fila++) {
+      banderaTarjetaHabilitada = true;
+      for (byte i = 0; i < 4; i++) {
 
-      if (tarjetaLeida[i] != tarjetasHabilitadas[fila][i]) {
-        banderaTarjetaHabilitada = false;
+        if (tarjetaLeida[i] != tarjetasHabilitadas[fila][i]) {
+          banderaTarjetaHabilitada = false;
+          break;
+        }
+      }
+
+      if (banderaTarjetaHabilitada == true) {
         break;
       }
     }
+
+    if (banderaTarjetaHabilitada == true) {
+      Serial.print("ACCESO PERMITIDO");
+    } else {
+      Serial.print("ACCESO DENEGADO");
+    }
+
+    // Halt PICC
+    rfid.PICC_HaltA();
+
+    // Stop encryption on PCD
+    rfid.PCD_StopCrypto1();
   }
 
-  if (banderaTarjetaHabilitada == true) {
-    Serial.print("ACCESO PERMITIDO");
-  } else {
-    Serial.print("ACCESO DENEGADO");
+  if (Serial.available() > 0) {
+    char datoRecibido = Serial.read();
+
+    if (datoRecibido == 'c') {
+      Serial.println("Entre a configuraciòn: \n\n'a': Dar de alta.\n'b': Dar de baja.");
+    }
+    if (datoRecibido == 'a') {
+      //Si doy de alta tengo que agregar a mi lista el nuevo tag o tarjeta leida
+      Serial.println("Quiere dar de alta");
+    }
+    if (datoRecibido == 'b') {
+      //Si doy de baja tengo que sacar de mi lista el tag o tarjeta y subir todo para arriba, cosa de agregar siempre al final.
+      Serial.println("Quiere dar de baja");
+    }
   }
-
-
-
-  // Halt PICC
-  rfid.PICC_HaltA();
-
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
 }
 
 
